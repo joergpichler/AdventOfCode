d0c306f288184a21794eeaeead83a8482bc36200
Jörg Pichler <joerg.pichler@gmail.com>
2022-01-05 14:47:25 +0100

    Adapt prepare_day.py

--- prepare_day.py
+++ prepare_day.py
@@ -17,15 +17,17 @@ if __name__ == '__main__':
 """
 
 def main(argv):
-    if len(argv) != 2:
+    if len(argv) != 3:
         raise Exception
-    day = int(argv[1])
+    year = int(argv[1])
+    day = int(argv[2])
     
-    dir = 'Day' + str(day)
-    os.mkdir(dir)
-    open(fr'{dir}\input.txt', 'w').close()
-    open(fr'{dir}\test.txt', 'w').close()
-    with open(fr'{dir}\{dir}.py', 'w') as f:
+    day = 'Day' + str(day).zfill(2)
+    
+    os.mkdir(fr'{year}/{day}')
+    open(fr'{year}/{day}\input.txt', 'w').close()
+    open(fr'{year}/{day}\test.txt', 'w').close()
+    with open(fr'{year}/{day}\{day}.py', 'w') as f:
         f.write(get_template())
 
 if __name__ == '__main__':
