41348e1a7264294154060168a6c57715b1ebf73a
Jörg Pichler <joerg.pichler@gmail.com>
2022-01-17 19:53:21 +0100

    Day 19 Pt 2

--- 2015/Day19/Day19.py
+++ 2015/Day19/Day19.py
@@ -33,6 +33,32 @@ def get_distinct_molecules(molecule: str, dict):
         idx += inc
     return len(molecules)
 
+def get_steps(molecule: str, dict: dict):
+    tmp = {}
+    for k,v in dict.items():
+        for i in v:
+            tmp[i] = k
+    dict = tmp
+    steps = 0
+    max_len = max([len(k) for k in dict])
+    
+    while molecule != 'e':
+        idx = 0
+        while idx < len(molecule):
+            for l in range(1,max_len+1):
+                sub_str = molecule[idx:idx+l]
+                if sub_str in dict and dict[sub_str] != 'e':
+                    molecule = replace_at(molecule, idx, dict[sub_str], len(sub_str))
+                    steps += 1
+                    idx += (len(dict[sub_str]) - 1)
+                    break
+                elif sub_str in dict and dict[sub_str] == 'e' and len(sub_str) == len(molecule):
+                    molecule = 'e'
+                    steps += 1
+            idx += 1
+        
+    return steps
+
 def main():
     assert replace_at("abc", 1, "dd") == "addc"
     assert replace_at("abcd", 1, "dd", 2) == "addd"
@@ -40,14 +66,17 @@ def main():
     molecule, dict = parse('test.txt')
     
     assert get_distinct_molecules(molecule, dict) == 4
+    assert get_steps(molecule, dict) == 3
     
     molecule, dict = parse('test2.txt')
     
     assert get_distinct_molecules(molecule, dict) == 7
+    assert get_steps(molecule, dict) == 6
     
     molecule, dict = parse('input.txt')
     
     print(f'Pt1: {get_distinct_molecules(molecule, dict)}')
-
+    print(f'Pt2: {get_steps(molecule, dict)}')
+    
 if __name__ == '__main__':
     main()
