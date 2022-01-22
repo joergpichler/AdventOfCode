7f397204abad577ad5ec1a3c066e083b4faf9869
Jörg Pichler <joerg.pichler@gmail.com>
2022-01-22 21:39:59 +0100

    Improve Day 24 and also port solution to C# just 4 fun

--- 2015/Day24/Day24.py
+++ 2015/Day24/Day24.py
@@ -2,68 +2,34 @@ import itertools
 import math
 from typing import List
 
-qte = -1
-length = -1
-
 def parse(file):
     with open(file, 'r') as f:
         return [int(l.strip()) for l in f]
 
-def calc_qte(results, length):
-    qte = -1
-    for p in results:
-        if len(p) != length:
-            continue
-        tmp_qte = math.prod(p)
-        if qte == -1:
-            qte = tmp_qte
-        elif tmp_qte < qte:
-            qte = tmp_qte
-    assert qte != -1
-    return qte
-
-def calc(results):
-    global length
-    global qte
-
-    min_len_results = min(len(x) for x in results)
-
-    if min_len_results > length:
-        return
-    elif min_len_results == length:
-        tmp_qte = calc_qte(results, min_len_results)
-        if tmp_qte < qte:
-            qte = tmp_qte
-            print(qte)
-    else:
-        length = min_len_results
-        qte = calc_qte(results, min_len_results)
-        print(qte)
-
-def find_combination(numbers: List[int], target_sum: int, depth: int, target_depth: int, results: List):
+def _calc_min_qte(numbers: List[int], target_sum: int, depth: int, target_depth: int):
+    
     if depth == target_depth:
-        if sum(numbers) == target_sum:
-            results.append(tuple(numbers))
-            calc(results)
-            return
-        else:
-            return
-
+        return True
+    
     for i in range(1, len(numbers)):
         for c in (x for x in itertools.combinations(numbers, i) if sum(x) == target_sum):
-            cpy = results.copy()
-            cpy.append(c)
-            find_combination(list(set(numbers)- set(c)), target_sum, depth + 1, target_depth, cpy)
+            remaining_numbers = list(set(numbers)- set(c))
+            if _calc_min_qte(remaining_numbers, target_sum, depth + 1, target_depth):
+                return math.prod(c)
+    
+    return False
+
+def calc_min_qte(numbers: List[int], split: int):
+    target_sum = sum(numbers) / split
+    if target_sum != int(target_sum):
+        raise Exception()
+    target_sum = int(target_sum)
+    return _calc_min_qte(numbers, target_sum, 1, split)
 
 def main():
     weights = parse('input.txt')
-    global length
-    length = len(weights)
-    target_weight = int(sum(weights) / 4)
-
-    results = []
-    find_combination(weights, target_weight, 1, 4, results)
-    # for whatever reason the first result is already the optimum ???! just run it and cancel when the first number is printed :)
+    print(f'Pt1: {calc_min_qte(weights, 3)}')
+    print(f'Pt2: {calc_min_qte(weights, 4)}')
 
 if __name__ == '__main__':
     main()
---
+++ 2015/Day24/Day24/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Combinatorics.Collections;
+
+namespace Day24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = File.ReadAllLines("input.txt").Select(x => int.Parse(x)).ToList();
+
+            Console.WriteLine($"{CalcMinQte(numbers, 3)}");
+            Console.WriteLine($"{CalcMinQte(numbers, 4)}");
+        }
+
+        static long CalcMinQte(IList<int> numbers, int targetSum, int depth, int targetDepth)
+        {
+            if (depth == targetDepth)
+            {
+                return 0;
+            }
+
+            for (var i = 1; i < numbers.Count; i++)
+            {
+                foreach (var c in new Combinations<int>(numbers, i, GenerateOption.WithoutRepetition).Where(x => x.Sum() == targetSum))
+                {
+                    var remainingNumbers = numbers.Except(c).ToList();
+                    if (CalcMinQte(remainingNumbers, targetSum, depth + 1, targetDepth) != -1)
+                    {
+                        var result = c.Select(x => (long) x).Aggregate((a, b) => a * b);
+                        return result;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        static long CalcMinQte(IList<int> numbers, int parts)
+        {
+            var sum = numbers.Sum() / parts;
+
+            if ((double)sum != (double)numbers.Sum() / parts)
+            {
+                throw new InvalidOperationException();
+            }
+
+            return CalcMinQte(numbers, sum, 1, parts);
+        }
+    }
+}
