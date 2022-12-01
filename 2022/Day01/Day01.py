39e838b957ef210530b86473c88ba02d22a76b32
Jörg Pichler <joerg.pichler@gmail.com>
2022-12-01 13:04:08 +0100

    Day01 Part02

--- 2022/Day01/Day01.py
+++ 2022/Day01/Day01.py
@@ -11,6 +11,9 @@ class Elf:
     def total_calories(self) -> int:
         return sum(self._calories)
 
+    def __repr__(self) -> str:
+        return str(self.total_calories)
+
 def parse(file):
     elves = []
     with open(file, 'r') as f:
@@ -21,20 +24,27 @@ def parse(file):
                 elf = Elf()
             else:
                 elf.add_calories(int(line))
+        elves.append(elf)
     return elves
 
 def get_highest_calorie_count(elves):
     return max(elves, key=attrgetter('total_calories')).total_calories
 
+def get_top3_calorie_count(elves):
+    sorted_list = sorted(elves, key=attrgetter('total_calories'), reverse=True)
+    return sorted_list[0].total_calories + sorted_list[1].total_calories + sorted_list[2].total_calories
+
 def main():
     elves = parse('test.txt')
-    max_calories = get_highest_calorie_count(elves)
-    assert max_calories == 24000
+    assert get_highest_calorie_count(elves) == 24000
+    assert get_top3_calorie_count(elves) == 45000
 
     elves = parse('input.txt')
     max_calories = get_highest_calorie_count(elves)
+    top3_calories = get_top3_calorie_count(elves)
     
     print(f'Pt1: {max_calories}')
+    print(f'Pt2: {top3_calories}')
 
     pass
 
