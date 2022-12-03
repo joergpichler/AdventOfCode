a96ef8364004053ba168dc0ff53c2953f0477c75
Jörg Pichler <joerg.pichler@gmail.com>
2022-12-03 11:09:50 +0100

    Day 03 Part 02

--- 2022/Day03/Day03.py
+++ 2022/Day03/Day03.py
@@ -2,7 +2,7 @@ def parse(file):
     with open(file, 'r') as f:
         return [l.strip() for l in f]
 
-def get_item(l: str):
+def get_item_1(l: str):
     if len(l) % 2 != 0:
         raise Exception
     a = list(l[:len(l)//2])
@@ -16,14 +16,36 @@ def get_value(item: str):
         val = ord(item) - ord('a') + 1
     return val
 
-def main():
-    data = parse('input.txt')
+def get_item_2(data: list[str]):
+    if len(data) != 3:
+        raise Exception
+    r = (set(data[0]) & set(data[1]) & set(data[2])).pop()
+    return r
+
+def get_results(file):
+    data = parse(file)
     sum = 0
     for d in data:
-        item = get_item(d)
+        item = get_item_1(d)
         value = get_value(item)
         sum += value
-    print(sum)
+    sum2 = 0
+    for i in range(0, len(data), 3):
+        d = data[i:i+3]
+        item = get_item_2(d)
+        value = get_value(item)
+        sum2 += value
+    return (sum, sum2)
+
+def main():
+    
+    results = get_results('test.txt')
+    assert results[0] == 157
+    assert results[1] == 70
+
+    results = get_results('input.txt')
+    print(f'Pt1: {results[0]}')
+    print(f'Pt2: {results[1]}')
 
 if __name__ == '__main__':
     main()
