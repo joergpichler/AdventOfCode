4ccc81009f46a98e3e50ea53dd5a176ef8d0f9d3
Jörg Pichler <joerg.pichler@gmail.com>
2022-12-11 10:46:14 +0100

    Day 11 Part 01

---
+++ 2022/Day11/Day11.py
@@ -0,0 +1,89 @@
+import re
+import math
+
+class Item:
+
+
+    def __init__(self, value) -> None:
+        self.value = value
+
+    
+    def apply_func(self, func) -> None:
+        self.value = func(self.value)
+
+
+    def is_dividable_by(self, val):
+        return self.value % val == 0
+
+    
+    def __repr__(self) -> str:
+        return f'{self.value}'
+
+
+class Monkey:
+
+
+    def __init__(self, lines) -> None:
+        self._parse(lines)
+        self.items_inspected = 0
+
+
+    def _parse(self, lines):
+        self.number = int(re.search(r'\d+', lines[0]).group(0))
+        self.items = [Item(int(x)) for x in re.findall(r'\d+', lines[1])]
+        self.func = eval(compile(f'lambda old: {lines[2].split("=")[1].strip()}', 'generated.py', 'eval'))
+        self.div_test = int(re.search(r'divisible by (\d+)', lines[3]).group(1))
+        self.true_case = int(re.search(r'If true: throw to monkey (\d+)', lines[4]).group(1))
+        self.false_case = int(re.search(r'If false: throw to monkey (\d+)', lines[5]).group(1))
+
+
+    def __repr__(self) -> str:
+        return f'Monkey {self.number}: {" ".join((str(i) for i in self.items))}'
+
+
+def parse(file):
+    monkeys = []
+    with open(file, 'r') as f:
+        l = f.readline()
+        while(l):
+            l = l.strip()
+            if l.startswith("Monkey"):
+                lines = [l]
+                for _ in range(5):
+                    lines.append(f.readline().strip())
+                monkeys.append(Monkey(lines))
+            l = f.readline()
+
+    return { m.number: m for m in monkeys }
+
+
+def play(monkeys, rounds, divide_by_three = True):
+    for _ in range(rounds):
+        for i in range(len(monkeys)):
+            monkey = monkeys[i]
+            while(len(monkey.items) > 0):
+                monkey.items_inspected += 1
+                item = monkey.items.pop(0)
+                item.apply_func(monkey.func)
+                if divide_by_three:
+                    item.apply_func(lambda x: math.floor(x / 3))
+                if item.is_dividable_by(monkey.div_test):
+                    target_monkey = monkeys[monkey.true_case]
+                else:
+                    target_monkey = monkeys[monkey.false_case]
+                target_monkey.items.append(item)
+    l = list(sorted((m.items_inspected for m in monkeys.values()), reverse=True))
+    return l[0] * l[1]
+
+
+def main():
+    monkeys = parse('test.txt')
+    assert play(monkeys, 20) == 10605
+    #assert play(monkeys, 10000, False) == 2713310158
+
+    monkeys = parse('input.txt')
+    print(f'{play(monkeys, 20)}')
+    pass
+
+if __name__ == '__main__':
+    main()
