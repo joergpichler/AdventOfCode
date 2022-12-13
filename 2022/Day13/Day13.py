769ad97084ce3052b88642f61d72613048200fe3
Jörg Pichler <joerg.pichler@gmail.com>
2022-12-13 18:34:49 +0100

    Day 13 Part 02

--- 2022/Day13/Day13.py
+++ 2022/Day13/Day13.py
@@ -1,9 +1,10 @@
-class Pair:
+from functools import cmp_to_key
+
+class Packet:
 
 
-    def __init__(self, l1, l2) -> None:
-        self.l1 = self._parse(l1)
-        self.l2 = self._parse(l2)
+    def __init__(self, l) -> None:
+        self.l = self._parse(l)
 
 
     def _parse(self, l):
@@ -45,70 +46,80 @@ class Pair:
         return int(token)
 
 
-    def is_in_order(self):
-        return self._is_in_order(self.l1, self.l2)
-
-    def _is_in_order(self, e1, e2):
-        result = None
-        if isinstance(e1, int) and isinstance(e2, int):
-            if e1 < e2:
-                return True
-            if e1 == e2:
-                return None
-            return False
-        elif isinstance(e1, list) and isinstance(e2, list):
-            for i in range(max(len(e1), len(e2))):
-                elem1 = e1[i] if len(e1) > i else None
-                elem2 = e2[i] if len(e2) > i else None
-                if elem1 is None and elem2 is not None:
-                    return True
-                if elem1 is not None and elem2 is None:
-                    return False
-                result = self._is_in_order(elem1, elem2)
-                if result is not None:
-                    return result
-        elif isinstance(e1, int) and isinstance(e2, list):
-            result = self._is_in_order([e1], e2)
-            if result is not None:
-                return result
-        elif isinstance(e1, list) and isinstance(e2, int):
-            result = self._is_in_order(e1, [e2])
-            if result is not None:
-                return result
-        else:
-            raise Exception
-        return result
-
-
 def parse(file):
-    packets = []
+    cache = []
     with open(file, 'r') as f:
-        while True:
-            l1 = f.readline()
-            l2 = f.readline()
-            f.readline()
-            if not l1 or not l2:
-                break
-            packets.append(Pair(l1.strip(), l2.strip()))
-    return packets
+        for l in f:
+            l = l.strip()
+            if l != '':
+                cache.append(l)
+    return [Packet(c) for c in cache]
 
+
+def compare(e1, e2):
+    result = 0
+    if isinstance(e1, Packet) and isinstance(e2, Packet):
+        return compare(e1.l, e2.l)
+    if isinstance(e1, int) and isinstance(e2, int):
+        if e1 < e2:
+            return -1
+        if e1 == e2:
+            return 0
+        return 1
+    elif isinstance(e1, list) and isinstance(e2, list):
+        for i in range(max(len(e1), len(e2))):
+            elem1 = e1[i] if len(e1) > i else None
+            elem2 = e2[i] if len(e2) > i else None
+            if elem1 is None and elem2 is not None:
+                return -1
+            if elem1 is not None and elem2 is None:
+                return 1
+            result = compare(elem1, elem2)
+            if result != 0:
+                return result
+    elif isinstance(e1, int) and isinstance(e2, list):
+        result = compare([e1], e2)
+        if result != 0:
+            return result
+    elif isinstance(e1, list) and isinstance(e2, int):
+        result = compare(e1, [e2])
+        if result != 0:
+            return result
+    else:
+        raise Exception
+    return result
 
 def pt1(packets):
     ctr = 0
     sum = 0
-    for p in packets:
+    for i in range(0, len(packets), 2):
         ctr += 1
-        if p.is_in_order():
+        p1 = packets[i]
+        p2 = packets[i + 1]
+        result = compare(p1, p2)
+        if result < 0:
             sum += ctr
     return sum
 
+def pt2(packets):
+    tmp = list(packets)
+    p1 = Packet('[[2]]')
+    p2 = Packet('[[6]]')
+    tmp.append(p1)
+    tmp.append(p2)
+
+    tmp = sorted(tmp, key=cmp_to_key(compare))
+
+    return (tmp.index(p1) + 1) * (tmp.index(p2) + 1)
 
 def main():
     packets = parse('test.txt')
     assert pt1(packets) == 13
+    assert pt2(packets) == 140
     
     packets = parse('input.txt')
     print(f'{pt1(packets)}')
+    print(f'{pt2(packets)}')
     pass
 
 
