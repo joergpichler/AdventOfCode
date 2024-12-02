97095a116af7af959aa14b4c43eb03014148bd91
Jörg Pichler <joerg.pichler@gmail.com>
2024-12-02 07:58:41 +0100

    Display execution time

--- 2024/AdventOfCode2024Kt/src/main/kotlin/base/Puzzle.kt
+++ 2024/AdventOfCode2024Kt/src/main/kotlin/base/Puzzle.kt
@@ -2,7 +2,6 @@ package de.pichlerj.base
 
 import java.nio.file.Paths
 import kotlin.io.path.createDirectory
-import kotlin.io.path.createTempDirectory
 import kotlin.io.path.exists
 
 abstract class Puzzle<TInput, TOutput>(private val day: Int, private val year: Int) {
@@ -29,22 +28,32 @@ abstract class Puzzle<TInput, TOutput>(private val day: Int, private val year: I
 
     fun runTest(): Puzzle<TInput, TOutput> {
         val input = getTestData()
+        println("Test data")
         run(input)
         return this
     }
 
     fun run() {
         val input = inputCache.getInput(day, year).trimEnd()
+        println("Input data")
         run(input)
     }
 
     private fun run(input: String): Puzzle<TInput, TOutput> {
         var parsedInput = parse(input)
-        val resultPart01 = solvePart01(parsedInput)
-        println(resultPart01)
+        val resultPart01 = measureExecutionTime { solvePart01(parsedInput) }
+        println("Pt1: ${resultPart01.first} (${resultPart01.second} ms)")
         parsedInput = parse(input)
-        val resultPart02 = solvePart02(parsedInput)
-        println(resultPart02)
+        val resultPart02 = measureExecutionTime { solvePart02(parsedInput) }
+        println("Pt2: ${resultPart02.first} (${resultPart02.second} ms)")
         return this
+    }
+
+    private fun <T> measureExecutionTime(block: () -> T): Pair<T, Long> {
+        val startTime = System.currentTimeMillis()
+        val result = block()
+        val endTime = System.currentTimeMillis()
+        val executionTime = endTime - startTime
+        return Pair(result, executionTime)
     }
 }
